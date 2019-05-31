3a97a0210ab86533980d0bd4293fefe30a71727d
dkzhang <527656264@qq.com>
2019-05-31 14:31:27 +0800

    Add mainProcessor.go

--- chassisDriverForRobot/mainProcessor/mainProcessor.go
+++ chassisDriverForRobot/mainProcessor/mainProcessor.go
@@ -1,6 +1,7 @@
 package mainProcessor
 
 import (
+	"Robot2019/chassisDriverForRobot/socketCommunication"
 	"sync"
 	"time"
 )
@@ -8,22 +9,33 @@ import (
 type MainProcessor struct {
 	//robotStatusListenerMap map[string](chan robotStatus.CallbackTopic)
 	//inChan                 chan string
+	serverIP string
+
+	commandChan chan socketCommunication.CommandStruct
+	resultChan  chan socketCommunication.CommandResultStruct
+	cancelChan  chan interface{}
 }
 
 func (mp *MainProcessor) run() {
+	go socketCommunication.SocketManagement(mp.serverIP, mp.commandChan, mp.resultChan, mp.cancelChan)
 
+	//go PrintResultChan(resultChan)
 }
 
 var ptr *MainProcessor = nil
 var once sync.Once
-var inChanSize = 1024
 var timeout = 10 * time.Second
+var chanSize int = 1024
 
 func TypeCallbackProcessorFactory() *MainProcessor {
 	once.Do(func() {
 		ptr = &MainProcessor{
 			//robotStatusListenerMap: make(map[string](chan robotStatus.CallbackTopic)),
 			//inChan:                 make(chan string, inChanSize),
+			serverIP:    "192.168.10.10:31001",
+			commandChan: make(chan socketCommunication.CommandStruct, chanSize),
+			resultChan:  make(chan socketCommunication.CommandResultStruct, chanSize),
+			cancelChan:  make(chan interface{}),
 		}
 		go ptr.run()
 	})
