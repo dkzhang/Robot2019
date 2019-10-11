d5860f639134748f74ea81b8560875ace89a270d
dkzhang@Office.PC <527656264@qq.com>
2019-10-11 15:55:26 +0800

    add InspectionLogRecord_string string version for using in Redis and JSON.

--- dataServer/myDatabase/structure/inpspectionLogRecord.go
+++ dataServer/myDatabase/structure/inpspectionLogRecord.go
@@ -1,7 +1,9 @@
 package structure
 
 import (
+	"Robot2019/myUtil"
 	"encoding/json"
+	"strconv"
 	"time"
 )
 
@@ -13,6 +15,28 @@ type InspectionLogRecord struct {
 	TextContent  string
 	ImageUrl     string
 }
+
+type InspectionLogRecord_string struct {
+	InspectionID string
+	RecordID     string
+	Level        string
+	DateTime     string
+	TextContent  string
+	ImageUrl     string
+}
+
+func (ilr *InspectionLogRecord) ConvertTo_string() InspectionLogRecord_string {
+	return InspectionLogRecord_string{
+		InspectionID: strconv.FormatInt(ilr.InspectionID, 10),
+		RecordID:     strconv.FormatInt(ilr.RecordID, 10),
+		Level:        ilr.Level,
+		DateTime:     myUtil.FormatTime(ilr.DateTime),
+		TextContent:  ilr.TextContent,
+		ImageUrl:     ilr.ImageUrl,
+	}
+}
+
+//strconv.FormatFloat(v, 'g', -1, 64)
 
 func MyMakeJSON() (string, error) {
 	ilr := InspectionLogRecord{
