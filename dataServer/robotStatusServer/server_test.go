b0f505654313b4216ba3234d317fa2fa2b6f8e5d
dkzhang <ubuntu@dell3520.laptop>
2019-10-22 15:33:33 +0800

    modify server_test

--- dataServer/robotStatusServer/server_test.go
+++ dataServer/robotStatusServer/server_test.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"Robot2019/myUtil"
+	"context"
 	_ "context"
 	"encoding/json"
+	"log"
 	_ "log"
 	"testing"
 	"time"
 
 	pb "Robot2019/dataServer/robotStatusServer/grpc"
 	"github.com/gomodule/redigo/redis"
-	_ "google.golang.org/grpc"
+	"google.golang.org/grpc"
 )
 
 func TestServer_GetRobotStatus(t *testing.T) {
 	//连接redis容器，读取相关状态信息
-	c, err := redis.Dial("tcp", "myRedis001:6379")
+	redisConn, err := redis.Dial("tcp", "myRedis001:6379")
 	if err != nil {
 		t.Fatalf("redis dial error: %v", err)
 		return
 	}
-	defer c.Close()
+	defer redisConn.Close()
 
 	theStatus := pb.RobotStatusReply{
 		MoveStatus:      "Running",
@@ -40,39 +42,38 @@ func TestServer_GetRobotStatus(t *testing.T) {
 		t.Fatalf("json marshal error: %v", theStatus)
 	}
 
-	result, err := redis.String(c.Do("SET", "CurrentRobotStatus", theJSON))
+	result, err := redis.String(redisConn.Do("SET", "CurrentRobotStatus", theJSON))
 	if err != nil {
 		t.Fatalf("Set CurrentRobotStatus error: %v", err)
 		return
 	}
 	t.Logf("SET result = %v", result)
 
-	//for i := 0; i < 20; i++ {
-	//	t.Logf("sleep: %d \n", i)
-	//}
+	for i := 0; i < 20; i++ {
+		t.Logf("sleep: %d \n", i)
+	}
 
 	/////////////////////////////////
 	// Set up a connection to the server.
-	//address := "localhost:50061"
-	//
-	//conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	//if err != nil {
-	//	log.Fatalf("did not connect: %v", err)
-	//}
-	//defer conn.Close()
-	////c := pb.NewGreeterClient(conn)
-	//c := pb.NewRobotStatusServiceClient(conn)
-	//c := pb.NewRobotStatusServiceClient(conn)
-	//
-	//// Contact the server and print out its response.
-	//
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	//defer cancel()
-	//r, err := c.GetRobotStatus()
-	////r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	//if err != nil {
-	//	log.Fatalf("could not greet: %v", err)
-	//}
-	//log.Printf("Greeting: %s", r.GetMessage())
+	address := "localhost:50061"
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewRobotStatusServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GetRobotStatus(ctx, &pb.RobotStatusRequest{Tag: myUtil.FormatTime(time.Now())})
+
+	if err != nil {
+		log.Fatalf("could not reply: %v", err)
+	}
+	log.Printf("reply = %v", r)
 
 }
