a90dcd5617df0e23cadc04f04271d2fcc2751afb
dkzhang@Office.PC <527656264@qq.com>
2019-11-25 14:40:51 +0800

    add server and client for laser light service.

---
+++ applicationDriverForRobot/laserLight/client/client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "Robot2019/applicationDriverForRobot/laserLight/grpc"
+	"google.golang.org/grpc"
+)
+
+func SwitchLaserLight(turnOn bool) {
+	/////////////////////////////////
+	// Set up a connection to the server.
+	//address := "localhost:50061"
+	address := "192.168.1.109:50051"
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	log.Printf("grpc.Dial OK!")
+	defer conn.Close()
+
+	c := pb.NewLaserLightServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*3)
+	log.Printf("context.WithTimeout() OK!")
+	defer cancel()
+	r, err := c.SwitchLaserLight(ctx, &pb.LaserLightRequest{TurnOn: turnOn})
+	if err != nil {
+		log.Fatalf("could not reply: %v", err)
+	}
+	log.Printf("reply = %v", r)
+}
---
+++ applicationDriverForRobot/laserLight/server/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"Robot2019/myUtil"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	//"os"
+	//"os/exec"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
+
+	pb "Robot2019/applicationDriverForRobot/laserLight/grpc"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedLaserLightServiceServer
+}
+
+func (s *server) ControlTheLifter(ctx context.Context, in *pb.LaserLightRequest) (*pb.LaserLightReply, error) {
+	log.Printf("Received: %v", in.GetTurnOn())
+
+	//use go-rpio
+	err := rpio.Open()
+	if err != nil {
+		return &pb.LaserLightReply{
+			ErrorMessage: err.Error(),
+		}, nil
+	}
+	defer rpio.Close()
+
+	pin := rpio.Pin(4)
+	pin.Output() // Output mode
+
+	turnOn := in.GetTurnOn()
+	if turnOn == true {
+		pin.High()
+	} else {
+		pin.Low()
+	}
+
+	return &pb.LaserLightReply{}, nil
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterLaserLightServiceServer(s, &server{})
+	fmt.Printf("Begin to serve %s \n", myUtil.FormatTime(time.Now()))
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
