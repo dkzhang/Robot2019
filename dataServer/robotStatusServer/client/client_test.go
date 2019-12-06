1634f59bfa0fe170080abffee60c702fd3ee0519
dkzhang@Office.PC <527656264@qq.com>
2019-12-06 14:47:56 +0800

    update Dockerfile

---
+++ dataServer/robotStatusServer/client/client_test.go
@@ -0,0 +1,12 @@
+package client
+
+import "testing"
+
+func TestGetRobotStatus(t *testing.T) {
+	reply, err := GetRobotStatus()
+	if err != nil {
+		t.Errorf("GetRobotStatus error: %v", err)
+	} else {
+		t.Logf("GetRobotStatus reply = %v", *reply)
+	}
+}
--- dataServer/robotStatusServer/server/server.go
+++ dataServer/robotStatusServer/server/server.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"Robot2019/cache"
