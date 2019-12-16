8356ce7b4c66051364b3259639b71cffcf1e7a7e
dkzhang@Office.PC <527656264@qq.com>
2019-12-16 16:53:23 +0800

    update missionPlanning.json

--- applicationDriverForRobot/lifterControl/client/client_test.go
+++ applicationDriverForRobot/lifterControl/client/client_test.go
@@ -2,13 +2,12 @@ package client
 
 import (
 	"testing"
-	"time"
 )
 
 func TestLifterControl(t *testing.T) {
-	LifterControl(1000 * 20)
-	time.Sleep(time.Second * 3)
-	LifterControl(-1000 * 22)
+	//LifterControl(1000 * 10)
+	//time.Sleep(time.Second * 3)
+	LifterControl(-1000 * 35)
 }
 
 //docker run -p 50051:50071 --rm --device /dev/gpiomem:/dev/mem  -e theApp="./lifterControl/server/server.go" registry.cn-beijing.aliyuncs.com/dkzhang/robot2019-app-go-arm32v7
